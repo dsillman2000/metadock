1e3a9d2313f49dd8f55ede5ad56e55f481482ae5
David Sillman <dsillman2000@gmail.com>
2023-11-24 22:42:50 -0800

    fixing precommit

---
+++ .metadock/metadock-build-docs.sh
@@ -0,0 +1,20 @@
+# install poetry project
+
+echo "Installing poetry project..."
+poetry install
+
+# metadock build README
+
+echo "Metadock-building README.md..."
+poetry run metadock build -s README
+cp .metadock/generated_documents/README.md README.md
+
+# metadock build Intellisense snippets
+
+echo "Metadock-building Intellisense snippets..."
+poetry run metadock build -s jinja-md
+cp .metadock/generated_documents/jinja-md.code-snippets .vscode/jinja-md.code-snippets
+
+# capture all changes
+
+git add -A
